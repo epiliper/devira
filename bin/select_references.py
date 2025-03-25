121b243023de8d72ea5fdce5dda64d5a9bb800c3
epiliper <epiliper@outlook.com>
2025-03-25 13:32:27 -0700

    revert: "feat: favors longest ref among ref_tags"
    This reverts commit 8a8dad7c1c83666052509d173319f17931069581:
    Adding more weight to genome length over genome ANI during reference
    selection resulted in gappy assemblies with way more Ns. We arguably
    don't need longer references anways since we depend on contigs for
    extension.

--- bin/select_references.py
+++ bin/select_references.py
@@ -50,7 +50,7 @@ def select_refs(sample_identifier, dist_tsv, ani_thres, align_ref_thres):
 
     # sort SKANI db hits first by fraction of ref genome covered, then by ANI
     data.sort(key = lambda row: (
-        float(row["Ref_90_ctg_len"]),
+        float(row["Total_bases_covered"]),
         float(row["ANI"]),
         float(row["Align_fraction_ref"])
         ), reverse = True)
